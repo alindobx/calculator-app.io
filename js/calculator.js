a3e2b3daddfe231f888abd9b42b38640b4594d0b
alindobx <tonyromebx@gmail.com>
2020-02-04 00:13:31 -0500

    ticket add EvventListner on decimal button

--- js/calculator.js
+++ js/calculator.js
@@ -3,30 +3,42 @@ const getClearEntryButton = document.getElementById('clear-entry');
 const getNumbersPlace = document.getElementById('numbers-place');
 getClearEntryButton.addEventListener('click',()=>{
     getNumbersPlace.innerHTML = '';
+    count = 0;
+    integer = [];
 });
 document.querySelector("#buttons-numbers").
 addEventListener('click', selectNumbers );
 
 // display each number in the numbers place
 // each number that is clicked its value will be rendered in the numbers place
-let stored;
+let integer =[];
+let getClickedSymbol;
+
 function selectNumbers (e) {
-    stored = e.target.value;
-    placeNumber()
+    getClickedSymbol = e.target.value;
+    console.log(`Symbol that was click ${getClickedSymbol}`);
+    operationSwitcher()
+}
+let count = 0;
+function operationSwitcher (){
+    count = ++count;
+    if((integer[0] ==='.') && (count < 4)){
+        placeNumber()
+    }else if(
+        (getClickedSymbol !== ".")
+        && (count < 10)
+        && (integer[0] !== '.') ) {
+        placeNumber()
+    }else if( (getClickedSymbol === '.')
+        && (count < 2) ) {
+        placeNumber()
+    }
 }
 
-const interger =[];
-let count = 0;
 function placeNumber() {
     const displayNumber = document.createElement('span');
-    displayNumber.innerHTML = stored;
-    count = ++count;
-    console.log(count);
-    if(count < 10) {
-        interger.push(stored);
-        getNumbersPlace.append(displayNumber);
-    }else{
-        return null
-    }
+    displayNumber.innerHTML =  getClickedSymbol;
+    getNumbersPlace.append(displayNumber);
+    integer.push(getClickedSymbol);
 }
 
